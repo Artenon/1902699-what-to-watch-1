1189396ccba6870990922ccca3b118ffdc294cee
Artem <artem_1parygin_2@mail.ru>
2022-09-20 09:39:35 +0500

    Create Main Page

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,18 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const data : {
+  name: string,
+  genre: string,
+  year: number
+} = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>,
 );
---
+++ project/src/pages/mainContent/mainContent.tsx
@@ -0,0 +1,241 @@
+import PromoFilmCard from '../../components/promoFilmCard/promoFilmCard';
+import SmallFilmCard from '../../components/smallFilmCard/smallFilmCard';
+
+type MainContentProps = {
+  name: string,
+  genre: string,
+  year: number
+}
+
+function MainContent({name, genre, year} : MainContentProps) : JSX.Element {
+  return (
+    <>
+      <div className="visually-hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+          <symbol id="add" viewBox="0 0 19 20">
+            <title>+</title>
+            <desc>Created with Sketch.</desc>
+            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+              <polygon id="+" fill="#EEE5B5" points="10.777832 11.2880859 10.777832 19.5527344 8.41650391 19.5527344 8.41650391 11.2880859 0.627929688 11.2880859 0.627929688 8.92675781 8.41650391 8.92675781 8.41650391 0.662109375 10.777832 0.662109375 10.777832 8.92675781 18.5664062 8.92675781 18.5664062 11.2880859"/>
+            </g>
+          </symbol>
+          <symbol id="full-screen" viewBox="0 0 27 27">
+            <path fillRule="evenodd" clipRule="evenodd" d="M23.8571 0H16V3.14286H23.8571V11H27V3.14286V0H23.8571Z" fill="#FFF9D9" fillOpacity="0.7"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M27 23.8571V16H23.8571V23.8571H16V27H23.8571H27L27 23.8571Z" fill="#FFF9D9" fillOpacity="0.7"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 3.14286L0 11H3.14286L3.14286 3.14286L11 3.14286V0H3.14286H0L0 3.14286Z" fill="#FFF9D9" fillOpacity="0.7"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M3.14286 27H11V23.8571H3.14286L3.14286 16H0L0 23.8571V27H3.14286Z" fill="#FFF9D9" fillOpacity="0.7"/>
+          </symbol>
+          <symbol id="in-list" viewBox="0 0 18 14">
+            <path fillRule="evenodd" clipRule="evenodd" d="M2.40513 5.35353L6.1818 8.90902L15.5807 0L18 2.80485L6.18935 14L0 8.17346L2.40513 5.35353Z" fill="#EEE5B5"/>
+          </symbol>
+          <symbol id="pause" viewBox="0 0 14 21">
+            <symbol id="play-s" viewBox="0 0 19 19">
+              <path fillRule="evenodd" clipRule="evenodd" d="M0 0L19 9.5L0 19V0Z" fill="#EEE5B5" />
+            </symbol>
+            <title>Artboard</title>
+            <desc>Created with Sketch.</desc>
+            <g id="Artboard" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+              <polygon id="Line" fill="#EEE5B5" fillRule="nonzero" points="0 -1.11910481e-13 4 -1.11910481e-13 4 21 0 21"/>
+              <polygon id="Line" fill="#EEE5B5" fillRule="nonzero" points="10 -1.11910481e-13 14 -1.11910481e-13 14 21 10 21"/>
+            </g>
+          </symbol>
+        </svg>
+      </div>
+      <section className="film-card">
+        <div className="film-card__bg">
+          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <header className="page-header film-card__head">
+          <div className="logo">
+            <a className="logo__link" href="/">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <ul className="user-block">
+            <li className="user-block__item">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </div>
+            </li>
+            <li className="user-block__item">
+              <a className="user-block__link">Sign out</a>
+            </li>
+          </ul>
+        </header>
+
+        <PromoFilmCard
+          name={name}
+          genre={genre}
+          date={year}
+        />
+      </section>
+
+      <div className="page-content">
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+          <ul className="catalog__genres-list">
+            <li className="catalog__genres-item catalog__genres-item--active">
+              <a href="#" className="catalog__genres-link">All genres</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Comedies</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Crime</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Documentary</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Dramas</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Horror</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Kids & Family</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Romance</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Sci-Fi</a>
+            </li>
+            <li className="catalog__genres-item">
+              <a href="#" className="catalog__genres-link">Thrillers</a>
+            </li>
+          </ul>
+
+          <div className="catalog__films-list">
+            <SmallFilmCard
+              name='Fantastic Beasts: The Crimes of Grindelwald'
+              imgSrc='fantastic-beasts-the-crimes-of-grindelwald.jpg'
+            />
+
+            <SmallFilmCard
+              name='Bohemian Rhapsody'
+              imgSrc='bohemian-rhapsody.jpg'
+            />
+
+            <SmallFilmCard
+              name='Macbeth'
+              imgSrc='macbeth.jpg'
+            />
+
+            <SmallFilmCard
+              name='Aviator'
+              imgSrc='aviator.jpg'
+            />
+
+            <SmallFilmCard
+              name='We need to talk about Kevin'
+              imgSrc='we-need-to-talk-about-kevin.jpg'
+            />
+
+            <SmallFilmCard
+              name='What We Do in the Shadows'
+              imgSrc='what-we-do-in-the-shadows.jpg'
+            />
+
+            <SmallFilmCard
+              name='Revenant'
+              imgSrc='revenant.jpg'
+            />
+
+            <SmallFilmCard
+              name='Johnny English'
+              imgSrc='johnny-english.jpg'
+            />
+
+            <SmallFilmCard
+              name='Shutter Island'
+              imgSrc='shutter-island.jpg'
+            />
+
+            <SmallFilmCard
+              name='Pulp Fiction'
+              imgSrc='pulp-fiction.jpg'
+            />
+
+            <SmallFilmCard
+              name='No Country for Old Men'
+              imgSrc='no-country-for-old-men.jpg'
+            />
+
+            <SmallFilmCard
+              name='Snatch'
+              imgSrc='snatch.jpg'
+            />
+
+            <SmallFilmCard
+              name='Moonrise Kingdom'
+              imgSrc='moonrise-kingdom.jpg'
+            />
+
+            <SmallFilmCard
+              name='Seven Years in Tibet'
+              imgSrc='seven-years-in-tibet.jpg'
+            />
+
+            <SmallFilmCard
+              name='Midnight Special'
+              imgSrc='midnight-special.jpg'
+            />
+
+            <SmallFilmCard
+              name='War of the Worlds'
+              imgSrc='war-of-the-worlds.jpg'
+            />
+
+            <SmallFilmCard
+              name='Dardjeeling Limited'
+              imgSrc='dardjeeling-limited.jpg'
+            />
+
+            <SmallFilmCard
+              name='Orlando'
+              imgSrc='orlando.jpg'
+            />
+
+            <SmallFilmCard
+              name='Mindhunter'
+              imgSrc='mindhunter.jpg'
+            />
+
+            <SmallFilmCard
+              name='Midnight Special'
+              imgSrc='midnight-special.jpg'
+            />
+          </div>
+
+          <div className="catalog__more">
+            <button className="catalog__button" type="button">Show more</button>
+          </div>
+        </section>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default MainContent;
