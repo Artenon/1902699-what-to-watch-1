ede5d3d9ea5bfbf27b3e1dae46721b2ff9d9f22c
Artem <artem_1parygin_2@mail.ru>
2022-10-07 14:05:23 +0500

    Create video player for movies previews

--- project/src/components/filmCard/filmCard.tsx
+++ project/src/components/filmCard/filmCard.tsx
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom';
+import VideoPlayer from '../videoPlayer/videoPlayer';
 
 type FilmCardProps = {
   name: string;
   previewImage: string;
   id: number;
-  onMouseOver: (id: number) => void
+  previewVideoLink: string;
+  onMouseOver: (id: number) => void;
+  onMouseOut: () => void;
+  isActive: boolean;
 }
 
 function FilmCard(props: FilmCardProps): JSX.Element {
-  const {name, previewImage, id, onMouseOver} = props;
+  const {name, previewImage, id, previewVideoLink, onMouseOver, onMouseOut, isActive} = props;
 
   return (
     <article className="small-film-card catalog__films-card">
-      <div className="small-film-card__image" onMouseOver={() => onMouseOver(id)}>
-        <img src={previewImage} alt={name} width="280" height="175" />
+      <div
+        className="small-film-card__image"
+        onMouseOver={() => onMouseOver(id)}
+        onMouseOut={() => onMouseOut()}
+      >
+        { isActive
+          ? <VideoPlayer src={previewVideoLink} poster={previewImage} />
+          : <img src={previewImage} alt={name} width="280" height="175" /> }
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
