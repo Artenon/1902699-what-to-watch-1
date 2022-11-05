f05e5f066b97b85777cab23c556345ff737b3284
Artem <artem_1parygin_2@mail.ru>
2022-11-05 16:14:25 +0500

    add email and password validation and messages in signin page

--- project/src/pages/signIn/signIn.tsx
+++ project/src/pages/signIn/signIn.tsx
@@ -1,4 +1,4 @@
-import { useRef, FormEvent, useEffect } from 'react';
+import { useRef, FormEvent, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import Logo from '../../components/logo/logo';
 import { AuthorizationStatus, AppRoute } from '../../const';
@@ -10,17 +10,54 @@ function SignIn() : JSX.Element {
   const dispatch = useAppDispatch();
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
 
+  const [isValidEmail, setIsValidEmail] = useState<boolean | null>(null);
+  const [isValidPassword, setIsValidPassword] = useState<boolean | null>(null);
+
   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const checkPassword = (password: string): boolean => {
+    const reg = /(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]{2,}/;
+    const result = reg.test(password);
+    result && setIsValidPassword(true);
+    return result;
+  };
+
+  const checkEmail = (email: string): boolean => {
+    const reg = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
+    return reg.test(email);
+  };
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
     if (emailRef.current !== null && passwordRef.current !== null) {
-      dispatch(loginAction({
-        email: emailRef.current.value,
-        password: passwordRef.current.value
-      }));
+      if (checkPassword(passwordRef.current.value) && checkEmail(emailRef.current.value)) {
+        dispatch(loginAction({
+          email: emailRef.current.value,
+          password: passwordRef.current.value
+        }));
+      }
+    }
+  };
+
+  const onEmailChange = (): void => {
+    if (emailRef.current !== null) {
+      if (checkEmail(emailRef.current.value)) {
+        setIsValidEmail(true);
+      } else {
+        setIsValidEmail(false);
+      }
+    }
+  };
+
+  const onPasswordChange = (): void => {
+    if (passwordRef.current !== null) {
+      if (checkPassword(passwordRef.current.value)) {
+        setIsValidPassword(true);
+      } else {
+        setIsValidPassword(false);
+      }
     }
   };
 
@@ -45,10 +82,11 @@ function SignIn() : JSX.Element {
           onSubmit={handleSubmit}
         >
           <div className="sign-in__fields">
-            <div className="sign-in__field">
+            <div className={`sign-in__field ${isValidEmail !== false || 'sign-in__field--error'}`}>
               <input
                 className="sign-in__input"
                 ref={emailRef}
+                onChange={() => onEmailChange()}
                 type="email"
                 placeholder="Email address"
                 name="user-email"
@@ -57,10 +95,17 @@ function SignIn() : JSX.Element {
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
-            <div className="sign-in__field">
+            {
+              isValidEmail !== false ||
+              <div className="sign-in__message">
+                Enter a valid email
+              </div>
+            }
+            <div className={`sign-in__field ${isValidPassword !== false || 'sign-in__field--error'}`}>
               <input
                 className="sign-in__input"
                 ref={passwordRef}
+                onChange={() => onPasswordChange()}
                 type="password"
                 placeholder="Password"
                 name="user-password"
@@ -69,6 +114,12 @@ function SignIn() : JSX.Element {
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
+            {
+              isValidPassword !== false ||
+              <div className="sign-in__message">
+                Password should contain at least one letter and one number
+              </div>
+            }
           </div>
           <div className="sign-in__submit">
             <button className="sign-in__btn" type="submit">Sign in</button>
