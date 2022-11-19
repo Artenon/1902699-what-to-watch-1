985fa2af4fe603a550a04ad17c6c0cc7ef3f1de1
Artem <artem_1parygin_2@mail.ru>
2022-11-19 12:06:38 +0500

    Testing: film rating level

--- project/src/const.ts
+++ project/src/const.ts
@@ -46,3 +46,11 @@ export enum NameSpace {
   User = 'USER',
   CurrentFilm = 'CURRENT_FILM'
 }
+
+export enum RatingLevel {
+  Awesome = 'Awesome',
+  VeryGood = 'Very good',
+  Good = 'Good',
+  Normal = 'Normal',
+  Bad = 'Bad'
+}
