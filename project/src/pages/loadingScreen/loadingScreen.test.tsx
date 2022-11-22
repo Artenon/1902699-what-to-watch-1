dfde354c81863ae72eac9bee6ef056c3a8c660d3
Artem <artem_1parygin_2@mail.ru>
2022-11-22 16:22:28 +0500

    Testing: LoadingScreen

---
+++ project/src/pages/loadingScreen/loadingScreen.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import HistoryRouter from '../../components/historyRouter/historyRouter';
+import LoadingScreen from './loadingScreen';
+
+describe('Component: LoadingScreen', () => {
+  it('Should render correctly', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <HistoryRouter history={history}>
+        <LoadingScreen />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByTestId('loading')).toBeInTheDocument();
+  });
+});
