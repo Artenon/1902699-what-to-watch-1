1a0c547b04f5b0d4da1f464fde48404bab50b02b
Artem <artem_1parygin_2@mail.ru>
2022-11-25 10:03:39 +0500

    Refactor: player test

--- project/src/pages/player/player.test.tsx
+++ project/src/pages/player/player.test.tsx
@@ -2,6 +2,7 @@ import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { Provider } from 'react-redux';
+import { Store } from '@reduxjs/toolkit';
 import { Action } from '@reduxjs/toolkit';
 import MockAdapter from 'axios-mock-adapter';
 import thunk, { ThunkDispatch } from 'redux-thunk';
@@ -14,16 +15,27 @@ import { makeFakeFilm } from '../../utils/mocks';
 import { AppRoute } from '../../const';
 import { createAPI } from '../../services/api';
 
-describe('Component: Player page', () => {
-  const api = createAPI();
-  const mockAPI = new MockAdapter(api);
-  const middlewares = [thunk.withExtraArgument(api)];
-  const mockStore = configureMockStore<
-    State,
-    Action,
-    ThunkDispatch<State, typeof api, Action>
-    >(middlewares);
+const api = createAPI();
+const mockAPI = new MockAdapter(api);
+const middlewares = [thunk.withExtraArgument(api)];
+const mockStore = configureMockStore<
+  State,
+  Action,
+  ThunkDispatch<State, typeof api, Action>
+  >(middlewares);
+const history = createMemoryHistory();
 
+const TestApp = ({store}: {store: Store}) => (
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route path={`${AppRoute.Player}/:filmId`} element={ <Player /> } />
+      </Routes>
+    </HistoryRouter>
+  </Provider>
+);
+
+describe('Component: Player page', () => {
   it('Should render correctly, video.play or pause should be called when play button is clicked', async () => {
     const store = mockStore({
       CURRENT_FILM: {
@@ -31,7 +43,6 @@ describe('Component: Player page', () => {
         isLoading: false
       }
     });
-    const history = createMemoryHistory();
     history.push(`${AppRoute.Player}/1`);
 
     window.HTMLVideoElement.prototype.play = jest.fn();
@@ -42,13 +53,7 @@ describe('Component: Player page', () => {
       .reply(200);
 
     render(
-      <Provider store={store}>
-        <HistoryRouter history={history}>
-          <Routes>
-            <Route path={`${AppRoute.Player}/:filmId`} element={ <Player /> } />
-          </Routes>
-        </HistoryRouter>
-      </Provider>
+      <TestApp store={store} />
     );
 
     expect(screen.getByTestId('video')).toBeInTheDocument();
@@ -68,17 +73,10 @@ describe('Component: Player page', () => {
         isLoading: true
       }
     });
-    const history = createMemoryHistory();
     history.push(`${AppRoute.Player}/1`);
 
     render(
-      <Provider store={store}>
-        <HistoryRouter history={history}>
-          <Routes>
-            <Route path={`${AppRoute.Player}/:filmId`} element={ <Player /> } />
-          </Routes>
-        </HistoryRouter>
-      </Provider>
+      <TestApp store={store} />
     );
 
     expect(screen.getByTestId('loading')).toBeInTheDocument();
